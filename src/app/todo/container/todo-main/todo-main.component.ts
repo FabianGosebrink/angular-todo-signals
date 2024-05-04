3db9b67f7df07588b38d1908499953a7c9ae5c23
FabianGosebrink <fabian.gosebrink@outlook.com>
2024-05-04 11:01:28 +0200

    Updated to control flow

--- src/app/todo/container/todo-main/todo-main.component.ts
+++ src/app/todo/container/todo-main/todo-main.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TodoService } from '../../services/todo.service';
+import { Component, computed, inject } from '@angular/core';
+import { Todo } from '../../models/todo';
 import { TodoFormComponent } from '../../presentational/todo-form/todo-form.component';
 import { TodoListComponent } from '../../presentational/todo-list/todo-list.component';
-import { Todo } from '../../models/todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-main',
@@ -14,6 +14,12 @@ import { Todo } from '../../models/todo';
 })
 export class TodoMainComponent {
   private todoService = inject(TodoService);
+
+  count = computed(() => {
+    const allItems = this.todoService.todos();
+
+    return allItems.length;
+  });
 
   todos = this.todoService.todos;
 
